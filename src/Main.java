6503a4f7230a080baf9d1d9bdf05c8957ec461a9
slaqlqrcoisa <carlosnt254@gmail.com>
2023-08-13 18:23:15 -0300

    Atividade atualizada

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,16 @@
 public class Main {
     public static void main(String[] args) {
+        Pessoa p1 = new Pessoa("Carlos", "834.346.213-22", "54.156.324-5", "(11) 3463-2346", "(11) 5686-2463", 1, "Brigadeiro Jordão");
+        Pessoa p2 = new Pessoa("Rogério", "436.658.325-19", "25.346.346-6", "(11) 3453-6347", "(11) 4574-3463", 1, "Jardim Belval");
+        Piloto plt1 = new Piloto("Marcia", "323.455.568-64", "63.236.567-9", "(11) 4754-6793", "(11) 5684-3425", "Gonçalves Dias", 33);
+        Balao b1 = new Balao();
        
-        Pessoa p1;
-        Pessoa p2;
-        Piloto plt1;
-        Balao b1;
-        
-        p1 = new Pessoa();
-        p2 = new Pessoa();
-        plt1 = new Piloto();
-        b1 = new Balao();
-       
-        p1.nome = "Carlos";
-        p1.CPF = "834.346.213-22";
-        p1.RG = "54.156.324-5";
-        p1.telefone = "(11) 3463-2346";
-        p1.telefoneFamiliar = "(11) 5686-2463";
-        p1.assinaturaTermo = 1;
-        p1.endereço = "Brigadeiro Jordão";
-
-        p2.nome = "Rogério";
-        p2.CPF = "436.658.325-19";
-        p2.RG = "25.346.346-6";
-        p2.telefone = "(11) 3453-6347";
-        p2.telefoneFamiliar = "(11) 4574-3463";
-        p1.assinaturaTermo = 1;
-        p2.endereço = "Jardim Belval";
-
-        plt1.nome = "Marcia";
-        plt1.CPF = "323.455.568-64";
-        plt1.RG = "63.236.567-9";
-        plt1.telefone = "(11) 4754-6793";
-        plt1.telefoneFamiliar = "(11) 5684-3425";
-        plt1.endereço = "Gonçalves Dias";
-        plt1.num_reg_anac = 033;
-
-        b1.capacidade = 4;
-        b1.ID = 02;
-        b1.reg_regularidade = 01;
+        b1.setCapacidade(4);
+        b1.setID(2);
+        b1.setRegRegularidade(1);
 
         String resposta = b1.voar(new Combustivel());
 
         System.out.println(resposta);
-} }
+    }
+}
--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,12 +1,80 @@
-public class Pessoa{
-    // Atributos da Classe
-    public String nome;
-    public String CPF;
-    public String RG;
-    public String telefone;
-    public String telefoneFamiliar;
-    public Byte assinaturaTermo;
-    public String endereço;
-    // Métodos da Classe
-    // Sobrescrito do Método
+public class Pessoa {
+    
+    private String nome;
+    private String CPF;
+    private String RG;
+    private String telefone;
+    private String telefoneFamiliar;
+    private int assinaturaTermo;
+    private String endereço;
+    
+    // Constructor
+    public Pessoa(String nome, String CPF, String RG, String telefone, String telefoneFamiliar,
+                  int assinaturaTermo, String endereço) {
+        this.nome = nome;
+        this.CPF = CPF;
+        this.RG = RG;
+        this.telefone = telefone;
+        this.telefoneFamiliar = telefoneFamiliar;
+        this.assinaturaTermo = assinaturaTermo;
+        this.endereço = endereço;
+    }
+    
+    // Getters
+    public String getNome() {
+        return nome;
+    }
+    
+    public String getCPF() {
+        return CPF;
+    }
+    
+    public String getRG() {
+        return RG;
+    }
+    
+    public String getTelefone() {
+        return telefone;
+    }
+    
+    public String getTelefoneFamiliar() {
+        return telefoneFamiliar;
+    }
+    
+    public int getAssinaturaTermo() {
+        return assinaturaTermo;
+    }
+    
+    public String getEndereço() {
+        return endereço;
+    }
+    
+    // Setters
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+    
+    public void setRG(String RG) {
+        this.RG = RG;
+    }
+    
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    
+    public void setTelefoneFamiliar(String telefoneFamiliar) {
+        this.telefoneFamiliar = telefoneFamiliar;
+    }
+    
+    public void setAssinaturaTermo(int assinaturaTermo) {
+        this.assinaturaTermo = assinaturaTermo;
+    }
+    
+    public void setEndereço(String endereço) {
+        this.endereço = endereço;
+    }
 }
